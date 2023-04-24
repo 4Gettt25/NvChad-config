4c414c2409b60cb791595c7bf8c44c0aca04a3fa
4Gettt25 <126004052+4Gettt25@users.noreply.github.com>
2023-04-24 23:36:15 +0200

    Add files via upload

---
+++ nvchad/lua/custom/configs/overrides.lua
@@ -0,0 +1,76 @@
+local M = {}
+
+M.treesitter = {
+ lazy = true;
+  ensure_installed = {
+    "vim",
+    "lua",
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "tsx",
+    "c",
+    "markdown",
+    "markdown_inline",
+    "json",
+    "json5",
+  },
+  indent = {
+    enable = true,
+    -- disable = {
+    --   "python"
+    -- },
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "deno",
+    "prettier",
+
+    -- c/cpp stuff
+    "clangd",
+    "clang-format",
+    --java 
+    "jdtls",
+    "java-debug-adapter",
+    --json
+    "json-lsp",
+    "jsonlint",
+    "js-debug-adapter",
+
+    --linter 
+    "codespell",
+    "eslint_d",
+    "misspell",
+    "",
+    "",
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  git = {
+    enable = true,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+return M
---
+++ nvchad/lua/custom/init.lua
@@ -0,0 +1,16 @@
+-- local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
+
+-- vscode format i.e json files
+--vim.g.vscode_snippets_path = "custom.configs.java"
+
+-- snipmate format 
+vim.g.snipmate_snippets_path = "~/.config/nvchad/lua/custom/configs/snippets"
+
+-- lua format 
+--vim.g.lua_snippets_path = vim.fn.stdpath "custom.configs.snippets"
